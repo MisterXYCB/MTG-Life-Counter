6f4c5a200a31efce4bdac201cad3ae6f369067b8
MisterXYCB <ycb2134@gmail.com>
2024-04-15 01:43:48 +0200

    some changes

--- src/app/1v1/page.tsx
+++ src/app/1v1/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import LifeCounter from "@/components/life-counter";
+import LeftRotatedLifeCounter from "@/components/left-rotated-life-counter";
+import RightRotatedLifeCounter from "@/components/right-rotated-life-counter";
 
 export default function oneVSone() {
   return (
-    <div className="grid grid-cols-2">
-      <LifeCounter className="h-[50vw] w-[100vh] left-0 rotate-90" />
-      <LifeCounter className="h-[50vw] w-[100vh] -rotate-90" />
+    <div className="absolute grid grid-cols-2 w-full h-full gap-0 ">
+      <LeftRotatedLifeCounter className="absolute top-0 origin-top-left" />
+      <RightRotatedLifeCounter className="absolute top-0 origin-top-left" />
     </div>
   );
 }
---
+++ src/components/left-rotated-life-counter.tsx
@@ -0,0 +1,81 @@
+'use client'
+
+import { useState, useEffect } from "react";
+import { Colorful } from "@uiw/react-color";
+const tinycolor = require("tinycolor2");
+
+export default function LeftRotatedLifeCounter({ className, heightPercentage = 100, widthPercentage = 50 }: { className?: string, heightPercentage?: number, widthPercentage?: number }) {
+    const [color, setColor] = useState("rgb(239, 68, 68)");
+    const [backgroundColor, setBackgroundColor] = useState("rgb(254, 202, 202)");
+    const [buttonColor, setButtonColor] = useState("rgb(248, 113, 113)");
+    const [colorMenuVisible, setColorMenuVisibility] = useState(false);
+    const [life, setLife] = useState(20);
+    const [commanderDamage, setCommanderDamage] = useState(0);
+    const [height, setHeight] = useState(50);
+    const [width, setWidth] = useState(100);
+
+
+    useEffect(() => {
+        function handleResize() {
+            const newHeight = window.innerWidth * (widthPercentage / 100);
+            const newWidth = window.innerHeight * (heightPercentage / 100);
+            setHeight(newHeight);
+            setWidth(newWidth);
+        }
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, [heightPercentage, widthPercentage]);
+    
+    function changeColor(inputColor: any) {
+        let newColor = tinycolor(inputColor);
+        if (newColor.isValid()) {
+            let colorStr = newColor.toHexString();
+            setColor(colorStr);
+            if (newColor.getBrightness() > 180) {
+                setBackgroundColor(newColor.darken(20).toHexString());
+                setButtonColor(newColor.darken(10).toHexString());
+            } else {
+                setBackgroundColor(newColor.lighten(20).toHexString());
+                setButtonColor(newColor.lighten(10).toHexString());
+            }
+        }
+    }
+
+    function changeLifeTotal(change: number) {
+        const newLife = life + change;
+            setLife(newLife);
+    }
+
+    function changeCommanderDamage(change: number) {
+        const newCommanderDamage = commanderDamage + change;
+            setCommanderDamage(newCommanderDamage);
+    }
+
+
+    return (
+        <div className={`absolute flex flex-col items-center justify-center rotate-90 ${className}`} style={{backgroundColor: backgroundColor, color: color, width: width, height: height, left: (height)}}>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,600,0,0"/>
+            <div className="absolute w-9 h-9 rounded-full top-4 right-4" style={{backgroundColor: buttonColor}} onClick={(event) => {setColorMenuVisibility(!colorMenuVisible)}}><span className="material-symbols-outlined scale">palette</span></div>
+            <div className="text-xs">Player</div>
+            <Colorful style={colorMenuVisible ? {position: "fixed", visibility:"visible"} : {visibility: "hidden", position: "fixed"}} disableAlpha={true} color={color} onChange={(newColor) => {changeColor(newColor.hex)}} rotation={90}/>
+            <div
+                className={`flex items-center justify-between w-64 h-24 text-4xl font-black rounded-full `}
+                style={{color: color, background: backgroundColor}}
+            >
+                <button className={`w-12 h-12 text-white rounded-full`} style={{backgroundColor: buttonColor}} onClick={(event) => {changeLifeTotal(-1)}}>-</button>
+                <div>{life}</div>
+                <button className={`w-12 h-12 text-white rounded-full`} style={{backgroundColor: buttonColor}} onClick={(event) => {changeLifeTotal(1)}}>+</button>
+            </div>
+            <div
+                className={`flex items-center justify-between pb-10 w-44 h-18 text-3xl font-medium rounded-full`}
+                style={{color: color, background: backgroundColor}}
+            >
+                <button className={`w-10 h-10 text-white rounded-full`} style={{backgroundColor: buttonColor}} onClick={(event) => {changeCommanderDamage(-1)}}>-</button>
+                <div>{commanderDamage}</div>
+                <button className={`w-10 h-10 text-white rounded-full`} style={{backgroundColor: buttonColor}} onClick={(event) => {changeCommanderDamage(1)}}>+</button>
+            </div>
+            <div className="text-xs">Deck</div>
+        </div>
+    );
+}
